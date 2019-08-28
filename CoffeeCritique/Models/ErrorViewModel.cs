f9277c619b33e00961eac49c2be664649dfb105e
Ibrahim Maga <imaga75@hotmail.com>
2019-08-28 11:52:48 -0700

    Add project files.

---
+++ CoffeeCritique/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CoffeeCritique.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
